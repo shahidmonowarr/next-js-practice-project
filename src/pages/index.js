77de6323001e0fd8f49bdbe3301628a42e49ef50
shahidmonowarr <shahidmonowar21@gmail.com>
2023-01-28 00:04:24 +0600

    tailwind and daisyui installation and setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-                <title>Blog Home</title>
-            </Head>
+        <title>Blog Home</title>
+      </Head>
       <h1>Hello, Mono</h1>
       <Link href="/blog">
         <button>Blog</button>
       </Link>
-      <br/>
-      <Image src="/marketing.jpg" width={400} height={400} ></Image>
+      <br />
+      <br />
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+      <br />
+      <br />
+      <br />
+      <Image src="/marketing.jpg" width={400} height={400}></Image>
     </div>
-  )
+  );
 }
