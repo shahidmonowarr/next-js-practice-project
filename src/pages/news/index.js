93789dac099f9c92d8b8daec36efd40c2d2252db
shahidmonowarr <shahidmonowar21@gmail.com>
2023-01-30 22:53:49 +0600

    built in css support explore

---
+++ src/pages/news/index.js
@@ -0,0 +1,11 @@
+import styles from '../../styles/News.module.css';
+
+const News = () => {
+    return (
+        <div className={styles.box}>
+            <h2>This is the news page</h2>
+        </div>
+    );
+};
+
+export default News;
